cfdee1e68679d5bd9f8a918722f864b1e66cf2a9
Murad <muradmustafayev03@gmail.com>
2023-02-06 22:27:12 +0400

    Cell and Player classes

--- main.py
+++ main.py
@@ -1,19 +1,42 @@
 import random
 
 
+class Cell:
+    def __init__(self, value):
+        self.value = value
+
+    def __repr__(self):
+        return str(self.value)
+
+
+class Player:
+    def __init__(self, name, position=0):
+        self.name = name
+        self.position = position
+
+    @staticmethod
+    def roll_dice(num_dices=2):
+        return sum([random.randint(1, 6) for _ in range(num_dices)])
+
+    def move(self, dice_roll):
+        self.position = (self.position + dice_roll) % len(Monopoly.board)
+
+
 class Monopoly:
-    def __init__(self, num_players):
-        self.board = [0, 10, 0, 20, 0, 30, 0, 40, 0, 50, 0, 60, 0, 70, 0, 80, 0, 90, 0, 100]
-        self.players = [{"position": 0, "name": f"Player {i}"} for i in range(1, num_players + 1)]
+    board = [Cell(value) for value in range(0, 400, 10)]
 
-    def play_turn(self, player):
-        input(f"{player['name']}, press enter to roll the dice: ")
-        dice_roll = random.randint(1, 6) + random.randint(1, 6)
-        player["position"] = (player["position"] + dice_roll) % len(self.board)
-        print(f"{player['name']} rolled a {dice_roll} and landed on {self.board[player['position']]}")
+    @staticmethod
+    def play_turn(player):
+        input(f'{player.name}, press enter to roll the dice: ')
+        dice_roll = player.roll_dice()
+        player.move(dice_roll)
+        print(f'{player.name} rolled a {dice_roll} and landed on {Monopoly.board[player.position]}')
 
 
-game = Monopoly(int(input("Enter number of players: ")))
-while True:
-    for player in game.players:
-        game.play_turn(player)
+if __name__ == '__main__':
+    num_players = int(input('Enter number of players: '))
+    players = [Player(f'Player {i}') for i in range(1, num_players + 1)]
+    game = Monopoly()
+    while True:
+        for player in players:
+            game.play_turn(player)
