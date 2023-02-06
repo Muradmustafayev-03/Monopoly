aaf74249dba1cf2bac733c61c42cb4d789cde2a4
Murad <muradmustafayev03@gmail.com>
2023-02-07 01:00:22 +0400

    collecting rent

--- monopoly.py
+++ monopoly.py
@@ -1,4 +1,5 @@
 from board import BOARD
+from cell import Cell
 from player import Player
 
 
@@ -8,18 +9,23 @@ class Monopoly:
         return player.money >= cost
 
     @staticmethod
-    def play_turn(player: Player):
-        print(f'\n{player.name}\nMoney: {player.money}\nProperty: {player.property}')
-        input('Press enter to roll the dice: ')
-        dice_roll = player.roll_dice()
-        player.move(dice_roll)
-        cell = BOARD[player.position]
-        print(f'{player.name} rolled a {dice_roll} and landed on {cell.name}')
+    def collect_rent(player: Player, cell: Cell):
+        if cell.owner is None:
+            return
+        if cell.owner == player:
+            return
+        player.money -= cell.rent
+        cell.owner.money += cell.rent
+        print(f'{player.name} paid {cell.rent} to {cell.owner.name}')
 
-        if cell.value == 0 or cell.owner:
+    @staticmethod
+    def buy_property(player: Player, cell: Cell):
+        if cell.value == 0:
             return
         if player.money < cell.value:
             print(f"{player.name}, you don't have enough money to buy {cell.name} for {cell.value}")
+            return
+        if cell.owner is not None:
             return
         choice = input(f'Do you want to buy {cell.name} for {cell.value}? (y/n): ')
         if choice.lower() == 'y':
@@ -30,5 +36,13 @@ class Monopoly:
         else:
             print(f'{player.name} decided not to buy {cell.name}')
 
+    def play_turn(self, player: Player):
+        print(f'\n{player.name}\nMoney: {player.money}\nProperty: {player.property}')
+        input('Press enter to roll the dice: ')
+        dice_roll = player.roll_dice()
+        player.move(dice_roll)
+        cell = BOARD[player.position]
+        print(f'{player.name} rolled a {dice_roll} and landed on {cell.name}')
 
-
+        self.collect_rent(player, cell)
+        self.buy_property(player, cell)
