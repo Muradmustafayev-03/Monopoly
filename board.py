b224dac4ab75bf9caae1819bbba4875a5318bc05
Murad <muradmustafayev03@gmail.com>
2023-02-07 01:59:14 +0400

    board

--- board.py
+++ board.py
@@ -1,16 +1,27 @@
 from cell import Cell
 
-BOARD = [
-    Cell('Go', 0, '', 0),
-    Cell('Mediterranean Avenue', 60, 'purple', 2),
-    Cell('Community Chest', 0, '', 0),
-    Cell('Baltic Avenue', 60, 'purple', 4),
-    Cell('Income Tax', 0, '', 0),
-    Cell('Reading Railroad', 200, 'railroad', 0),
-    Cell('Oriental Avenue', 100, 'light blue', 6),
-    Cell('Chance', 0, '', 0),
-    Cell('Vermont Avenue', 100, 'light blue', 6),
-    Cell('Connecticut Avenue', 120, 'light blue', 8),
-    Cell('Jail', 0, '', 0),
-    # and so on
-]
+CELL_NAMES = ['Go', 'Mediterranean Ave', 'Community Chest', 'Baltic Ave', 'Income Tax', 'Reading Railroad',
+              'Oriental Ave', 'Chance', 'Vermont Ave', 'Connecticut Ave', 'Jail', 'St. Charles Place',
+              'Electric Company', 'States Ave', 'Virginia Ave', 'Pennsylvania Railroad', 'St. James Place',
+              'Community Chest', 'Tennessee Ave', 'New York Ave', 'Free Parking', 'Kentucky Ave', 'Chance',
+              'Indiana Ave', 'Illinois Ave', 'B&O Railroad', 'Atlantic Ave', 'Ventnor Ave', 'Water Works',
+              'Marvin Gardens', 'Go to Jail', 'Pacific Ave', 'North Carolina Ave', 'Community Chest',
+               'Pennsylvania Ave', 'Short Line', 'Chance', 'Park Place', 'Luxury Tax', 'Boardwalk']
+
+CELL_VALUES = [0, 60, 0, 60, 0, 200, 100, 0, 100, 120, 0, 140, 150, 140, 160, 200, 180, 0, 180, 200, 0,
+               220, 0, 220, 240, 200, 260, 260, 150, 280, 0, 300, 300, 0, 320, 200, 0, 350, 0, 400]
+
+CELL_RENTS = [0, 2, 0, 4, 0, 25, 6, 0, 6, 8, 0, 10, 0, 10, 12, 25, 14, 0, 14, 16, 0, 18, 0, 18, 20, 25, 22, 22, 0, 24,
+              0, 26, 26, 0, 28, 0, 25, 35, 0, 50]
+
+CELL_COLORS = ['Go', 'Brown', 'Community Chest', 'Brown', 'Tax', 'Railroad',
+               'Light Blue', 'Chance', 'Light Blue', 'Light Blue', 'Jail', 'Pink',
+               'Utility', 'Pink', 'Pink', 'Railroad', 'Orange', 'Community Chest', 'Orange',
+               'Orange', 'Red', 'Chance', 'Red', 'Red', 'Railroad', 'Yellow', 'Yellow',
+               'Utility', 'Yellow', 'Jail', 'Green', 'Green', 'Community Chest',
+               'Green', 'Railroad', 'Dark Blue', 'Chance', 'Dark Blue', 'Tax', 'Dark Blue']
+
+
+BOARD = [Cell(name=CELL_NAMES[i], value=CELL_VALUES[i], rent=CELL_RENTS[i], color=CELL_COLORS[i])
+             for i in range(40)]
+
--- cell.py
+++ cell.py
@@ -1,8 +1,8 @@
 class Cell:
-    def __init__(self, name: str, value: int, color: str, rent: int):
+    def __init__(self, name: str, color: str, value: int, rent: int):
         self.name = name
-        self.value = value
         self.color = color
+        self.value = value
         self.rent = rent
         self.owner = None
 
