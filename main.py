ad423fe25cff1600bc7c1a3e239c1b4b5c944f82
Murad <muradmustafayev03@gmail.com>
2023-02-06 22:16:07 +0400

    multiple players

--- main.py
+++ main.py
@@ -2,17 +2,18 @@ import random
 
 
 class Monopoly:
-    def __init__(self):
-        self.board = list(range(40))
-        self.player_position = 0
+    def __init__(self, num_players):
+        self.board = [0, 10, 0, 20, 0, 30, 0, 40, 0, 50, 0, 60, 0, 70, 0, 80, 0, 90, 0, 100]
+        self.players = [{"position": 0, "name": f"Player {i}"} for i in range(1, num_players + 1)]
 
-    def play_turn(self):
-        input("Press enter to roll the dice: ")
+    def play_turn(self, player):
+        input(f"{player['name']}, press enter to roll the dice: ")
         dice_roll = random.randint(1, 6) + random.randint(1, 6)
-        self.player_position = (self.player_position + dice_roll) % len(self.board)
-        print("You rolled a", dice_roll, "and landed on", self.board[self.player_position])
+        player["position"] = (player["position"] + dice_roll) % len(self.board)
+        print(f"{player['name']} rolled a {dice_roll} and landed on {self.board[player['position']]}")
 
 
-game = Monopoly()
+game = Monopoly(int(input("Enter number of players: ")))
 while True:
-    game.play_turn()
+    for player in game.players:
+        game.play_turn(player)
