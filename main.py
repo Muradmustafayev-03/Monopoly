43c3088f3ffd816df5270de61f1cedcbb9f32881
Murad <muradmustafayev03@gmail.com>
2023-02-07 11:25:05 +0400

    auctions

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from monopoly import Monopoly
 def main():
     num_players = int(input('Enter number of players: '))
     players = [Player(input(f'Enter the name of the player {i}: ')) for i in range(1, num_players + 1)]
-    game = Monopoly()
+    game = Monopoly(players)
     while True:
         for player in players:
             game.play_turn(player)
--- monopoly.py
+++ monopoly.py
@@ -4,6 +4,9 @@ from player import Player
 
 
 class Monopoly:
+    def __init__(self, players: list[Player]):
+        self.players = players
+
     @staticmethod
     def collect_rent(player: Player, cell: Cell):
         if cell.owner is None:
@@ -32,6 +35,53 @@ class Monopoly:
         else:
             print(f'{player.name} decided not to buy {cell.name}')
 
+    def recursive_auction(self, bidder: Player or None, bid: int, player: Player, cell: Cell):
+        current_bid = bid
+        current_bidder = bidder
+
+        for p in self.players:
+            if p == player or p == current_bidder:
+                continue
+            if p.money < current_bid:
+                print(f"{p.name} doesn't have enough money to bid")
+                continue
+            bid = input(
+                f'{p.name}, do you want to bid on {cell.name}? Current bid is {current_bid}. '
+                f'Enter your bid or skip to pass: ')
+            if bid.lower() == '':
+                continue
+            bid = int(bid)
+            if bid <= current_bid:
+                print(f'{p.name}, your bid must be higher than {current_bid}')
+                continue
+            current_bid = bid
+            current_bidder = p
+            print(f'{p.name} bid {bid} on {cell.name}')
+
+        if current_bidder is None:
+            print(f'No one bid on {cell.name}, auction ended')
+        elif current_bid <= cell.value:
+            print(f'{cell.name} auction ended as no one bid higher than the starting bid')
+        elif current_bidder == bidder:
+            return current_bidder, current_bid
+        else:
+            return self.recursive_auction(current_bidder, current_bid, player, cell)
+
+    def sell_property_auction(self, player: Player, cell: Cell):
+        if cell not in player.property:
+            return
+
+        print(f'{player.name} is selling {cell.name} on auction starting bid is {cell.value}')
+
+        bidder, bid = self.recursive_auction(None, cell.value, player, cell)
+
+        bidder.money -= bid
+        player.money += bid
+        cell.owner = bidder
+        player.property.remove(cell)
+        bidder.property.append(cell)
+        print(f'{bidder.name} bought {cell.name} for {bid} from {player.name}')
+
     def play_turn(self, player: Player):
         print(f'\n{player.name}\nMoney: {player.money}\nProperty: {player.property}')
         input('Press enter to roll the dice: ')
@@ -48,4 +98,20 @@ class Monopoly:
         self.collect_rent(player, cell)
         self.buy_property(player, cell)
 
-        action = input('Press Enter to finish your turn: ')
+        while True:
+            action = input('Press Enter to finish your turn or input "s" to sell a property: ')
+
+            if not action:
+                break
+            if action == 's':
+                if not player.property:
+                    print("You don't posses any property")
+                    continue
+                for i, prop in enumerate(player.property):
+                    print(f'{i + 1}. {prop.name}')
+                choice = int(input('Enter the number of the property you want to sell: ')) - 1
+                if choice >= len(player.property) or choice < 0:
+                    print('Wrong input\n')
+                    continue
+
+                self.sell_property_auction(player, player.property[choice])
