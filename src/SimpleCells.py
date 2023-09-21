ad6dd264ce85bbee7592ecbb34a96bcf015f6419
Murad <muradmustafayev03@gmail.com>
2023-09-21 16:17:50 +0400

    created GoCell class

--- src/SimpleCells.py
+++ src/SimpleCells.py
@@ -13,3 +13,25 @@ class PassiveCell(AbstractCell):
     def action(self, *args, **kwargs):
         """Passive cells do not have any action."""
         pass
+
+
+class GoCell(AbstractCell):
+    """
+    Class for the Go cell. This cell gives money to the player.
+
+    Attributes:
+        name (str): The name of the cell.
+        earn_amount (int): The amount of money the player gets when he passes from this cell.
+    """
+
+    def __init__(self, name: str = "Go", earn_amount: int = 200):
+        super().__init__(0, name)
+        self._earn_amount = earn_amount
+
+    @property
+    def earn_amount(self):
+        return self._earn_amount
+
+    def action(self, player):
+        """The player gets money."""
+        player.add_money(self.earn_amount)
